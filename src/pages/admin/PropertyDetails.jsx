823842f7dd3ccce68fad87b4c9e02dd5f5412eee
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-03-27 10:52:02 +0100

    fixed: saved the state of property status in cookies

--- src/pages/admin/PropertyDetails.jsx
+++ src/pages/admin/PropertyDetails.jsx
@@ -58,6 +58,7 @@ export const PropertyDetails = () => {
 
   useEffect(() => {
     getSingleProperty();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [propertyId]);
 
   return (
